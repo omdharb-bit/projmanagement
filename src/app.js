4e3500a7fdb1bcbea50253d7bf27a70f68f57d94
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-10-07 16:48:54 +0530

    v

--- src/app.js
+++ src/app.js
@@ -1,27 +1,31 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
 //basic configuration
-app.use(express.json({ limi: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 //cors configuration
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTION"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
+// import the routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 app.get("/", (req, res) => {
-res.send("Welcome to basecampy")
-})
-
+  res.send("Welcome to basecampy");
+});
 
 export default app;
